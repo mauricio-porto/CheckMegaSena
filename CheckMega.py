340f96e8c7d17e88f1b890cdedbc05fcd65aec04
Mauricio Porto <mauriporto@gmail.com>
2018-10-21 11:21:35 -0300

    Adicionando o script CheckMega.py

---
+++ CheckMega.py
@@ -0,0 +1,91 @@
+import json
+from bs4 import BeautifulSoup
+import requests
+import zipfile
+
+url = 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_mgsasc.zip'
+z = requests.get(url)
+open('D_mgsasc.zip', 'wb').write(z.content)
+
+zipfile.ZipFile('D_mgsasc.zip').extractall()
+
+
+def send_email(subject, body):
+    import smtplib
+
+    gmail_user = 'fulano@gmail.com'
+    gmail_pwd = 'fulanoPwd'
+
+    FROM = 'fulano@gmail.com'
+    TO = '<fulano ou beltrano>@gmail.com'
+    SUBJECT = subject
+    TEXT = body
+
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, TO, SUBJECT, TEXT)
+
+    try:
+       server = smtplib.SMTP('smtp.gmail.com', 587)
+       server.ehlo()
+       server.starttls()
+       server.login(gmail_user, gmail_pwd)
+       server.sendmail(FROM, TO, message)
+       server.close()
+    except:
+       print('Algo deu errado no envio de email...')
+
+with open('apostas.json', 'r') as arq_apostas:
+    read_apostas = arq_apostas.read();
+
+apostas_json = json.loads(read_apostas)
+
+with open('d_megasc.htm', 'r', encoding="utf8", errors='ignore') as arq:
+    read_htm = arq.read();
+
+apostas = apostas_json["apostas"]
+sorteios = apostas_json["sorteios"]
+nr_acertos = []
+for n in range(len(sorteios)):
+    nr_acertos.append(0)
+nr_apostas = len(apostas)
+
+encerrou = False
+
+rawh = BeautifulSoup(read_htm, 'html.parser')
+
+rows = rawh.find_all('tr')
+for row in rows[:]:
+    first_td = row.find('td')
+    if(first_td is not None):
+      if(first_td.text.isnumeric()):
+          nr_sorteio = int(first_td.text)
+          if(nr_sorteio > sorteios[len(sorteios)-1]):
+              encerrou = True
+              break
+          tamu_dentro = False
+          for sorteio in sorteios[:]:
+              if(nr_sorteio == sorteio):
+                  tamu_dentro = True
+                  break
+          if(tamu_dentro):
+            data_sorteio = first_td.next_sibling.next_sibling
+            for n in range(len(sorteios)):
+                nr_acertos[n] = 0
+            dezena = data_sorteio
+            for d in range(6):
+                dezena = dezena.next_sibling.next_sibling
+                dezena_int = int(dezena.text)
+                for aposta in range(nr_apostas):
+                    for d in range(len(apostas[aposta])):
+                        if(dezena_int == apostas[aposta][d]):
+                            nr_acertos[aposta] += 1
+            for aposta in range(nr_apostas):
+                if(nr_acertos[aposta] == 4):
+                    send_email('QUADRA !', 'ACERTOU NO SORTEO ' + str(nr_sorteio))
+                elif(nr_acertos[aposta] == 5):
+                    send_email('QUINA !!', 'ACERTOU NO SORTEIO ' + str(nr_sorteio))
+                elif(nr_acertos[aposta] == 6):
+                    send_email('SENA !!!', 'ACERTOU NO SORTEIO ' + str(nr_sorteio))
+
+if(encerrou):
+    send_email('MEGASENA','Fazer novas apostas')
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CheckMegaSena
 Um "robô" para verificar suas apostas na MegaSena
+
+## Criar arquivo de apostas
+
+A forma mais simples é usar a linha de comando para gerar um único registro JSON com as apostas.
+Um exemplo vale mais que mil palavras:
+
+` echo '{"sorteios":[2088,2089,2090,2091],"apostas":[[1,2,3,4,5,6,7],[11,22,33,44,55,59]]}' > apostas.json `
+
+O que significa que você apostou em dois conjuntos de dezenas ([1,2,3,4,5,6,7] e [11,22,33,44,55,59]) para os sorteios 2088, 2089, 2090 e 2091
+
+
+
